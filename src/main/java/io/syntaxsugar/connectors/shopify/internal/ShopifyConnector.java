7bac3b21dce62107bafa8abfc560610b0eede0c5
Michael Jones <mjones@kotrs.com>
2021-06-21 12:04:26 -0400

    Added initial entry point

---
+++ src/main/java/io/syntaxsugar/connectors/shopify/internal/ShopifyConnector.java
@@ -0,0 +1,10 @@
+package io.syntaxsugar.connectors.shopify.internal;
+
+import org.mule.runtime.extension.api.annotation.Extension;
+import org.mule.runtime.extension.api.annotation.Operations;
+import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
+
+@Xml(prefix = "shopify")
+@Extension(name = "Shopify")
+public class ShopifyConnector {
+}
